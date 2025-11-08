8749479fccd8997b0fe15e91d15bdea8e5e2e1d6
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2025-11-08 23:23:32 +0100

    feat: ✨ [] nextJs15-lp

--- templates/NextJs/NextJs15-lp/src/ui/button.tsx
+++ templates/NextJs/NextJs15-lp/src/ui/button.tsx
@@ -40,7 +40,7 @@ function Button({
 }: React.ComponentProps<'button'> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
-  }) {
+  }): React.ReactElement {
   const Comp = asChild ? Slot : 'button';
 
   return <Comp data-slot='button' className={cn(buttonVariants({ variant, size, className }))} {...props} />;
